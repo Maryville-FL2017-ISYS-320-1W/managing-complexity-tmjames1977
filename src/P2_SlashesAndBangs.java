638f9d071bbf01c50b5fbf9aa764ad42bae88845
tmjames1977 <33112355+tmjames1977@users.noreply.github.com>
2017-11-13 22:51:44 -0600

    Completed Assignment

--- src/P2_SlashesAndBangs.java
+++ src/P2_SlashesAndBangs.java
@@ -1,7 +1,7 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Timothy James
+  	Date: 11/13/2017
 */
 
 
@@ -11,26 +11,62 @@
  * how many of the character is printed on each line? Choose your headings based on how you can break down
  * the figure.
   
-Line Number		| HEADING		| HEADING		| HEADING		| HEADING		| HEADING		|	
-
-
-
-
+Line Number		| Bangs		| Slashes		| HEADING		| HEADING		| HEADING		|	
+1				|	22		|	0
+2				|	18		|	4
+3				|	14		|	8
+4				|	10		|	12
+5				|	6		|	16
+6				|	2		|	20
 
 */
 
 /* 3. Using your analysis from above, in pseudocode describe the algorithm for how to draw the figure
- 
- 
- 
- 
+ for {
+ print 22 bangs.
+ }
+ for {
+ print 2 slashes.
+ print 18 bangs.
+ print 2 slashes.
+ }
+ for {
+ print 4 slashes.
+ print 14 bangs.
+ print 4 slashes.
+ }
+ for {
+ print 6 slashes.
+ print 10 bangs.
+ print 6 slashes.
+ }
+ for {
+ print 8 slashes.
+ print 6 bangs.
+ print 8 slashes.
+ }
+ for {
+ print 10 slashes.
+ print 2 bangs.
+ print 10 slashes.
+ }
  
  */
 
 public class P2_SlashesAndBangs {
 
-	public static void main(String[] args) {
-
+	public static void main (String[] args) {
+		for (int line = 6; line >= 1; line--) {
+			for (int i = 1; i <= (2 * (6 - line)); i++) { // write slashes
+				System.out.print("/");
+			}	
+			for (int i = 1; i <= (4 * line - 2); i++) { // write bangs
+				System.out.print("!");
+			}
+			for (int i = 1; i <= (2 * (6 - line)); i++) { // write slashes
+				System.out.print("/");
+			}
+			System.out.println();
+		}
 	}
-
 }
--- src/P5_ScalableBox.java
+++ src/P5_ScalableBox.java
@@ -2,41 +2,35 @@
 
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s): Timothy James
+	Date: 11/13/2017
 */
 
 public class P5_ScalableBox {
-
+	public static final int Sub_Height = 6;
+	public static final int Sub_Width = 10;
 	public static void main(String[] args) {
 		drawBoxCap();
 		drawBoxInsides();
 		drawBoxCap();
 	}
-	
-	public static void drawBoxCap() {
+		public static void drawBoxCap() {
 		System.out.print("+");
-		
-		for( int capDashIndex = 1; capDashIndex <= 8 - 2; capDashIndex++ ) {
+		for( int capDashIndex = 1; capDashIndex <= Sub_Width - 2; capDashIndex++ ) {
 			System.out.print("-");
 		}
-		
 		System.out.println("+");
 	}
-	
-	public static void drawBoxInsides() {
-		for( int insideRowIndex = 1; insideRowIndex <= 10 - 2; insideRowIndex++ ) {
+		public static void drawBoxInsides() {
+		for( int insideRowIndex = 1; insideRowIndex <= Sub_Height - 2; insideRowIndex++ ) {
 			drawBoxInsideLine();
 		}
 	}
-	
 	public static void drawBoxInsideLine() {
 		System.out.print("|");
-		
-		for( int insideRowIndex = 1; insideRowIndex <= 8 - 2; insideRowIndex++ ) {
+		for( int insideRowIndex = 1; insideRowIndex <= Sub_Width - 2; insideRowIndex++ ) {
 			System.out.print(".");
 		}
-		
 		System.out.println("|");
 	}
 
